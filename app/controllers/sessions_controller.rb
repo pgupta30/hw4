6ae44daeef8e6c80e3a33ea1784f7d23a242cf42
Prakhar Gupta <prakhar.gupta@kellogg.northwestern.edu>
2023-06-05 17:34:12 +0000

    2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,22 @@ class SessionsController < ApplicationController
 
   def create
   
-    end
+  @user = User.find_by({"email" => params["email"]})
+    
+  if @user
+      password_the_user_typed = params["password"]
+      password_in_the_database = @user["password"]
+      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+        
+        redirect_to "/places"
+    
+      else
+        
+        redirect_to "sessions/new"
+    
+      end
+  
+  end
 
   def destroy
   end
-end
-  
