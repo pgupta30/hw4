e8cdc46f6eee7c3887cbe5c5ae6315d059354d82
Prakhar Gupta <prakhar.gupta@kellogg.northwestern.edu>
2023-06-05 18:33:34 +0000

    3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,30 @@
 class SessionsController < ApplicationController
   def new
+    #render login form in sessions/new.html.erb
   end
 
   def create
-  
-  @user = User.find_by({"email" => params["email"]})
-    
-  if @user
-      password_the_user_typed = params["password"]
-      password_in_the_database = @user["password"]
-      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
-        
-        redirect_to "/places"
-    
-      else
-        
-        redirect_to "sessions/new"
-    
-      end
-  
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+    else
+    flash["notice"] = "Nope."
+    redirect_to "/sessions/new"
+    end
   end
-
-  def destroy
-  end
+end
+    # @user = User.find_by({"email" => params["email"]})
+    # if @user
+    #   password_the_user_typed = params["password"]
+    #   password_in_the_database = @user["password"]
+    #   if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+    #     flash["notice"] = "Yes!"
+    #     redirect_to "/places"
+    #   else
+    #     flash["notice"] = "Nope."
+    #     redirect_to "sessions/new"
+    #   end
+    # else
+      # flash["notice"] = "Nope."
+      # redirect_to "sessions/new"
+  #   end
+  # end
